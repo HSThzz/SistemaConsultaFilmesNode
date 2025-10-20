78bea94fbb206a2d7c3a5f08f83e8de45a26f9fb
Thiago Esmerio <thiagoesmerio123@gmail.com>
2025-10-20 20:19:26 -0300

    Criaçao da entidade filme

---
+++ src/models/filmes.ts
@@ -0,0 +1,14 @@
+class Filmes{
+    
+    id: number
+    original_title: string
+    overview: string
+    
+    constructor(id: number, original_title: string, overview: string){
+        this.id = id 
+        this.original_title = original_title
+        this.overview = overview
+    }
+
+
+}
