10ee2dd22c0adbf318909ea40c7fe5eaa30a92d5
Thiago Esmerio <thiagoesmerio123@gmail.com>
2025-10-20 19:22:19 -0300

    Implementando rotas para busca dos filmes

--- src/routes/filmes.routes.ts
+++ src/routes/filmes.routes.ts
@@ -1,6 +1,9 @@
 import { Router } from "express";
 import { getFilmes, addFilme } from "../controllers/filmesController";
 
+
+
+
 const router = Router();
 //rota get a partir de filmes
 router.get("/", getFilmes);
@@ -23,6 +26,60 @@ router.get("/now-playing", async (req, res)=>{
         console.log(erro)
     }
 })
+
+router.get("/popular", async (req, res)=>{
+    try{
+        const resposta = await fetch("https://api.themoviedb.org/3/movie/popular?api_key=YOUR_API_KEY&language=pt-BR&page=1", {
+            method: "GET",
+            headers: {
+                Authorization: "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhODdjOGFhYzYzYzJjNGNmMzA5MTQxMWJkYTc5NTJhNyIsIm5iZiI6MTc1ODcyOTUyOC4wODUsInN1YiI6IjY4ZDQxNTM4Njc3Y2EyNzE3ZjcxOWQyYSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.MjaVOiJsyUscImOv_MlS0_uDUu98MGsXlnV6v0wf7lc"
+
+            },
+        })
+        const dados = await resposta.json()
+        res.json(dados)
+        console.log(dados)
+    }catch(erro){
+        console.log(erro)
+    }
+})
+
+router.get("/:id", async (req, res)=>{
+    try{
+        const ID = req.params.id;
+        const resposta = await fetch("https://api.themoviedb.org/3/movie/"+ ID, {
+            method: "GET",
+            headers: {
+                Authorization: "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhODdjOGFhYzYzYzJjNGNmMzA5MTQxMWJkYTc5NTJhNyIsIm5iZiI6MTc1ODcyOTUyOC4wODUsInN1YiI6IjY4ZDQxNTM4Njc3Y2EyNzE3ZjcxOWQyYSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.MjaVOiJsyUscImOv_MlS0_uDUu98MGsXlnV6v0wf7lc"
+
+            },
+        })
+        const dados = await resposta.json()
+        res.json(dados)
+        console.log(dados)
+    }catch(erro){
+        console.log(erro)
+    }
+})
+router.get("/titulo/:titulo", async (req, res)=>{
+    try{
+        const MOVIE = req.params.titulo;
+        console.log("MOVIE: "+ MOVIE);
+        const resposta = await fetch("https://api.themoviedb.org/3/search/movie?api_key=YOUR_API_KEY&query="+MOVIE+"&language=pt-BR&page=1&api_key=YOUR_API_KEY&query=superman&language=pt-BR&page=1", {
+            method: "GET",
+            headers: {
+                Authorization: "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhODdjOGFhYzYzYzJjNGNmMzA5MTQxMWJkYTc5NTJhNyIsIm5iZiI6MTc1ODcyOTUyOC4wODUsInN1YiI6IjY4ZDQxNTM4Njc3Y2EyNzE3ZjcxOWQyYSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.MjaVOiJsyUscImOv_MlS0_uDUu98MGsXlnV6v0wf7lc"
+
+            },
+        })
+        const dados = await resposta.json()
+        res.json(dados)
+        console.log(dados)
+    }catch(erro){
+        console.log(erro)
+    }
+})
+
 
 
 
