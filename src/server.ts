0dc70abd70f37113402565f83e8d238541b4b1c4
Thiago Esmerio <thiagoesmerio123@gmail.com>
2025-10-21 22:36:44 -0300

    Ajudando server

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,15 @@
 import express from "express"
 
 import filmesRoutes from "./routes/filmes.routes"
+import loginRoutes from "./routes/authRoutes"
 
 const app = express();
 
 app.use(express.json())
 
-//rota principal e secundarias
-//app.use("/filmes", filmesRoutes)
 
 app.use("/filmes", filmesRoutes)
+app.use("/filmes", loginRoutes)
 
 const PORT = 3000;
 
